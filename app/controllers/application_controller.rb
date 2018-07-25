55988f45a9e3a9060b1d482783d1ea5dff1eb718
Jed Peek <jedpeek@yahoo.com>
2018-07-25 20:46:30 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
 
   get '/posts/:id' do
     binding.pry
-    @post = Post.find(params[:post][:id]).first
+    @post = Post.find(params[:id])
     erb :'show'
   end
 end
